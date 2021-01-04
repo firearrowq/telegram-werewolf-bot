687c5419ae478521c8b1d3894a2188fc792987f0
firearrowq <76500199+firearrowq@users.noreply.github.com>
2021-01-05 01:06:28 +0300

    Update config.example.js

--- config.example.js
+++ config.example.js
@@ -1,10 +1,10 @@
 var config = {
-  "token": "123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11",
+  "token": "1480132437:AAF9MGIVwY_C8F7sn75jMysh91U10cJBtTE",
   "update_type": "long-polling", // or "webhook"
-  "webhook": "https://www.example.com/<token>",
+  "webhook": "https://werewolfsbot.herokuapp.com/<token>",
   //"proxy": "http://127.0.0.1:8118",
   //"bot_name": "example_bot", // check bot name,
-  "admin_id": "35197423",
+  "admin_id": "1144225359",
   "db": {
     host: '127.0.0.1',
     port: 27017,
